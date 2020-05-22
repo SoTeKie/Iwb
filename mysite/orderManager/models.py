b66a1a75125ae4ba03529e23ff34fcc7e9b095e8
Kinno <kinno@Kinno.localdomain>
2020-05-22 11:08:10 +0200

    Plural for categories,registered admin and updated requirements and gitignore

--- mysite/orderManager/models.py
+++ mysite/orderManager/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = 'categories'
 
 class Item(models.Model):
     category = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='items')
